590e988c80c050dd7da98243af9ca2800cdde925
Newt Industries <info@newtindustries.com>
2019-12-12 10:26:41 -0600

    Created first set of required classes and class methods designated by user stories

---
+++ SweepstakesFolder/Sweepstakes/Contestant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+
+    }
+}
---
+++ SweepstakesFolder/Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        public Sweepstakes(string contestName)
+        {
+
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        public string PickWinner()
+        {
+            string winner = "winner";
+            return winner;
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
